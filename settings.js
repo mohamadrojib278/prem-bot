eaa3c98ba368866369e368546acb8acd681bd8e5
Mohamad <91793179+mohamadrojib278@users.noreply.github.com>
2024-09-12 22:55:09 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,232 @@
+const chalk = require("chalk")
+const fs = require("fs")
+
+global.autoTyping = true 
+global.autoRecord = false 
+global.autoblockmorroco = true 
+global.autokickmorroco = true 
+global.antispam = true
+global.wlcm = []
+global.wlcmm = []
+global.versi = '11.0.0'
+global.keyopenai = "pk-pIWAlRroXTOAigkWdHcYvmlmgzEQXuoMWbVAaLAVZswSRbEB" // Udah Expired Ganti Aja
+global.btz = 'btzraffxzer' // Free
+//===============SETTING MENU==================\\
+global.thumbnail = fs.readFileSync("./data/image/thumb.jpg")
+global.ig = '@raffxzer'
+global.yt = 'Fernazer'
+global.ttowner = '@fernazer'
+global.ownername = 'RaffX'
+global.owner = ['6281328139682'] // SETTING JUGA DI FOLDER DATABASE OWNER.json
+global.ownernomer = '6281328139682'
+global.socialm = 'GitHub: Fernazer'
+global.location = 'Indonesia' 
+//========================setting Payment=====================\\
+global.nodana = '085182675367' // KOSONG KAN JIKA TIDAK ADA
+global.nogopay = '085182675367' // KOSONG KAN JIKA TIDAK ADA 
+global.noovo = '085182675367' // KOSONG KAN JIKA TIDAK ADA
+//==================setting Payment Name===========================\\
+global.andana = 'Rafli' // KOSONG KAN JIKA TIDAK ADA
+global.angopay = 'Rafli' // KOSONG KAN JIKA TIDAK ADA
+global.anovo = 'Rafli' // KOSONG KAN JIKA TIDAK ADA
+//==================setting bot===========================\\
+global.botname = "Manz assisten"
+global.ownernumber = '6281328139682'
+global.botnumber = '6281328139682'
+global.ownername = 'RAFFX'
+global.ownerNumber = ["6281328139682@s.whatsapp.net"]
+global.ownerweb = ""
+global.websitex = ""
+global.gris = '`'
+global.wagc = "https://whatsapp.com/channel/0029VaZCS2aId7nOgboKPn1Q"
+global.saluran = "https://whatsapp.com/channel/0029VaZCS2aId7nOgboKPn1Q"
+global.themeemoji = '🪀'
+global.wm = "RAFFX | 6281328139682"
+global.botscript = 'DI YT: Fernazer https://youtube.com/Fernazer' //script link
+global.packname = "Sticker By"
+global.author = "\n\n\n\n\nCreate by RAFFX ID\nNo hape/wa : 6281328139682"
+global.creator = "6281328139682@s.whatsapp.net"
+//======================== CPANEL FITUR ===========================\\
+global.domain = 'https://ptero-yassxofc.vcloudx.my.id' // Isi Domain Lu jangan kasih tanda / di akhir link
+global.apikey = 'ptla_i9qEm15Auxn67mXPI3cp83bdxvypgpYGWr6YhqYsoMI' // Isi Apikey Plta Lu
+global.capikey = 'ptlc_SrvQiGEuxa9DExPls7BXGdTh3gbjKg9DnGlcL3dAmqJ' // Isi Apikey Pltc Lu
+//=========================================================//
+//Server create panel egg pm2
+global.apikey2 = '-' // Isi Apikey Plta Lu
+global.capikey2 = '-' // Isi Apikey Pltc Lu
+global.domain2 = '-' // Isi Domain Lu
+global.docker2 = "ghcr.io/cekilpedia/vip:sanzubycekil" //jangan di ubah
+
+global.eggsnya2 = '15' // id eggs yang dipakai
+global.location2 = '1' // id location
+//===========================//
+global.domainotp = "https://claudeotp.com/api"
+global.apikeyotp = "a395f97fe99f4fad0e790d10af518b9a"
+global.eggsnya = '15' // id eggs yang dipakai
+global.location3 = '1' // id location
+global.tekspushkon = ""
+global.tekspushkonv2 = ""
+global.tekspushkonv3 = ""
+global.tekspushkonv4 = ""
+global.hiasan2 = `	◦  `
+global.chat = 'Bangun Sayang Sudah Jam'
+//===========================//
+
+global.decor = {
+	menut: '❏═┅═━–〈',
+	menub: '┊•',
+	menub2: '┊',
+	menuf: '┗––––––––––✦',
+	hiasan: '꒦ ͝ ꒷ ͝ ꒦ ͝ ꒷ ͝ ꒦ ͝ ꒷ ͝ ꒦ ͝ ꒷ ͝ ꒦ ͝ ꒷ ͝ ꒦ ͝ ꒷ ͝ ꒦ ͝ ꒷ ͝ ꒦ ͝ ꒷',
+
+	menut: '––––––『',
+    menuh: '』––––––',
+    menub: '┊☃︎ ',
+    menuf: '┗━═┅═━––––––๑\n',
+	menua: '',
+	menus: '☃︎',
+
+	htki: '––––––『',
+	htka: '』––––––',
+	haki: '┅━━━═┅═❏',
+	haka: '❏═┅═━━━┅',
+	lopr: 'Ⓟ',
+	lolm: 'Ⓛ',
+	htjava: '❃'
+}
+
+//===========================//
+
+global.rpg = {
+    emoticon(string) {
+        string = string.toLowerCase()
+        let emot = {
+            level: '📊',
+            limit: '🎫',
+            health: '❤️',
+            exp: '✨',
+            atm: '💳',
+            money: '💰',
+            bank: '🏦',
+            potion: '🥤',
+            diamond: '💎',
+            common: '📦',
+            uncommon: '🛍️',
+            mythic: '🎁',
+            legendary: '🗃️',
+            superior: '💼',
+            pet: '🔖',
+            trash: '🗑',
+            armor: '🥼',
+            sword: '⚔️',
+            makanancentaur: "🥗",
+            makanangriffin: "🥙",
+            makanankyubi: "🍗",
+            makanannaga: "🍖",
+            makananpet: "🥩",
+            makananphonix: "🧀",
+            pickaxe: '⛏️',
+            fishingrod: '🎣',
+            wood: '🪵',
+            rock: '🪨',
+            string: '🕸️',
+            horse: '🐴',
+            cat: '🐱',
+            dog: '🐶',
+            fox: '🦊',
+            robo: '🤖',
+            petfood: '🍖',
+            iron: '⛓️',
+            gold: '🪙',
+            emerald: '❇️',
+            upgrader: '🧰',
+            bibitanggur: '🌱',
+            bibitjeruk: '🌿',
+            bibitapel: '☘️',
+            bibitmangga: '🍀',
+            bibitpisang: '🌴',
+            anggur: '🍇',
+            jeruk: '🍊',
+            apel: '🍎',
+            mangga: '🥭',
+            pisang: '🍌',
+            botol: '🍾',
+            kardus: '📦',
+            kaleng: '🏮',
+            plastik: '📜',
+            gelas: '🧋',
+            chip: '♋',
+            umpan: '🪱',
+            naga: "🐉",
+            phonix: "🦅",
+            kyubi: "🦊",
+            griffin: "🦒",
+            centaur: "🎠",
+            skata: '🧩'
+        }
+        let results = Object.keys(emot).map(v => [v, new RegExp(v, 'gi')]).filter(v => v[1].test(string))
+        if (!results.length) return ''
+        else return emot[results[0][0]]
+    }
+}
+
+//new
+global.prefix = ['','!','.','#','&']
+global.sessionName = 'session'
+global.hituet = 0
+//media target
+global.thum = fs.readFileSync("./data/image/thumb.jpg") //ur thumb pic
+global.log0 = fs.readFileSync("./data/image/thumb.jpg") //ur logo pic
+global.err4r = fs.readFileSync("./data/image/thumb.jpg") //ur error pic
+global.thumb = fs.readFileSync("./data/image/thumb.jpg") //ur thumb pic
+global.defaultpp = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60' //default pp wa
+
+//menu image maker
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
+//messages
+global.mess = {
+wait: 'FLIX BOT | JANGAN SPAM',
+   success: "Sukses",
+   on: "Sudah Aktif", 
+   off: "Sudah Off",
+   query: {
+       text: "Teks Nya Mana?",
+       link: "Link Nya Mana?",
+       rpg: "Admin menonaktifkan fitur *RPG Game* di grup ini!",
+   },
+   error: {
+       fitur: "Mohon Maaf Fitur Eror Silahkan Chat Developer Bot Agar Bisa Segera Diperbaiki",
+   },
+   block: {
+Bowner: `Maaf kak command 「 *Command* 」 telah di block oleh owner`,
+Bsystem: `Command 「 *Command* 」telah di block oleh system karena terjadi error`
+},
+   only: {
+       group: "Maaf Fitur Ini Hanya Bisa Digunakan Di Dalam Group",
+       private: "Maaf Fitur Ini Hanya Bisa Digunakan Di Dalam Private Chat",
+       owner: "Maaf Fitur Ini Hanya Bisa Digunakan Sama Owner Bot",
+       admin: "Maaf Fitur Ini Hanya Bisa Digunakan Sama Owner Bot",
+       badmin: "Maaf Kaya Nya Kakak Tidak Bisa Menggunakan Fitur Ini Di Karenakan Bot Bukan Admin Group",
+       premium: "ᴍᴀᴀғ ᴋᴀᴍᴜ ʙᴇʟᴏᴍ ᴍᴇɴᴊᴀᴅɪ ᴜsᴇʀ ᴘʀᴇᴍɪᴜᴍ ᴜɴᴛᴜᴋ ᴍᴇɴᴊᴀᴅɪ ᴘʀᴇᴍɪᴜᴍ sɪʟᴀᴋᴀɴ ʙᴇʟɪ ᴅɪ ᴏᴡɴᴇʀ ᴅᴇɴɢᴀɴ ᴄᴀʀᴀ ᴋᴇᴛɪᴋ  .ᴏᴡɴᴇʀ",
+   }
+}
+ 
+//documents variants
+global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+global.doc2 = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+global.doc3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+global.doc4 = 'application/zip'
+global.doc5 = 'application/pdf'
+global.doc6 = 'application/vnd.android.package-archive'
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
